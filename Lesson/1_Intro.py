e09dc2a80e55d719608da711982304cb28990957
Khushnood Ali Khan <khushnoodali287@gmail.com>
2026-08-28 00:11:14 +0500

    intro

--- Lesson/1_Intro.py
+++ Lesson/1_Intro.py
@@ -6,3 +6,12 @@ y = [1000, 1500, 1200, 1800]
 
 plt.plot(x, y)
 plt.show()
+
+# we can use numpy array since they are faster then ppython list
+import numpy as np
+
+x_axis = np.array([1, 2, 3, 4, 5, 6])
+y_axis = np.array([3.6, 3.5, 3.3, 3.5, 3.7, 3.8])
+
+plt.plot(x_axis, y_axis)
+plt.show()
